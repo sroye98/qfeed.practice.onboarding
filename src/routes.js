1e91349ce518d3579c6e2caf0df821931673ee98
Saachi Roye <saachi.roye@gmail.com>
2021-07-16 17:52:23 -0500

    added 404 page

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { Route, Switch } from 'react-router-dom';
 
 import Registration from './views/Registration';
 import ConfirmEmail from './views/ConfirmEmail';
+import NotFound from './views/404';
 
 const Routes = (props) => {
     return (
@@ -9,6 +10,7 @@ const Routes = (props) => {
             <Switch>
                 <Route exact path="/" component={Registration} />
                 <Route exact path="/confirm-email" component={ConfirmEmail} />
+                <Route path="*" component={NotFound} />
             </Switch>
         </div>
     );
